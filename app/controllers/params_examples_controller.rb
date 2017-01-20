f11742d0730927227c073657c49889ad32fa61b2
Mark Richardson <markeban@yahoo.com>
2017-01-20 13:33:48 +0200

    form example

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -9,4 +9,14 @@ class ParamsExamplesController < ApplicationController
     @zebra = params[:tiger]
     render "url_segment_action.html.erb"
   end
+
+  def form_params_action
+    render "form_params_url.html.erb"
+  end
+
+
+  def form_result_action
+    @message = params[:form_message]
+    render "form_result_action.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get "/query_params_url", to: 'params_examples#query_params_action'
   get "/url_segment_params_url/:tiger", to: 'params_examples#url_segment_action'
+
+  get "/form_params_url", to: 'params_examples#form_params_action'
+  post "/form_result_url", to: 'params_examples#form_result_action'
 end
